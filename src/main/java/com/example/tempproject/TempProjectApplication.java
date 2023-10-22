303de4c38600b6a575bca15e3ff347123261a3a3
Алексей Леонов <avleonov@diasoft.ru>
2023-10-22 10:11:18 +0300

    first commit

---
+++ src/main/java/com/example/tempproject/TempProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tempproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tempproject/TempProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.tempproject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
